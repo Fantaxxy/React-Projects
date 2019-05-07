87a3d882c4b7f3bbae758265240d5d71b06f9bf1
Jordan Byrd <jordan@baltimorehealthanalytics.com>
2019-05-07 11:06:28 -0400

    all react projects added

--- firstCom.js
+++ firstCom.js
@@ -10,4 +10,16 @@ class Hello extends React.Component {
     }
 }
 
+// using function componenets are simple to use, but i beinf replaces by react hooks which allows for full featured componenets
+
+// function Hello () {
+//     return (
+//         <div>
+//             <h1>Hello there!</h1>
+//             <h1>Hello there!</h1>       
+//             <h1>Hello there!</h1>
+//         </div>
+//     );
+// }
+
 ReactDOM.render(<Hello />, document.getElementById('root'));
